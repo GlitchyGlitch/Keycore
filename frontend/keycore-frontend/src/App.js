05b769ee332f98a7594241c6719ca29bbe4f54fe
Mateusz Jaśkiewicz <jaskiewiczteo@gmail.com>
2022-06-08 14:55:23 +0200

    Create basic design

--- frontend/keycore-frontend/src/App.js
+++ frontend/keycore-frontend/src/App.js
@@ -1,25 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import Toolbar from "@mui/material/Toolbar";
+import AddIcon from "@mui/icons-material/Add";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import CardContent from "@mui/material/CardContent";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
 
-function App() {
+const services = [
+  {
+    name: "Facebook",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Gmail",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "Instagram",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "GitHub",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "OHV",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+  {
+    name: "NameCheap",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  },
+];
+
+const ServiceCard = ({ name, description }) => {
+  return (
+    <Card variant="outlined" sx={{ maxWidth: 1920 }}>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
+      <Toolbar>
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ fontWeight: "bold" }}
+        >
+          KEY
+        </Typography>
+
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ fontWeight: "light" }}
+        >
+          CORE
+        </Typography>
+        <Box sx={{ flexGrow: 1 }} />
+        <IconButton color="inherit">
+          <AddIcon />
+        </IconButton>
+        <IconButton color="inherit">
+          <SearchIcon />
+        </IconButton>
+        <IconButton color="inherit">
+          <MoreIcon />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <Navbar />
+      <Container>
+        <Box sx={{ my: 4 }}>
+          <Grid container spacing={2}>
+            {services.map(({ name, description }, index) => (
+              <Grid item xs={12} md={4} xl={3} key={index.toString()}>
+                <ServiceCard name={name} description={description}/>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+      </Container>
     </div>
   );
-}
+};
 
 export default App;
